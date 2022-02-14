73abdfb0ab9af30cceccfa76dad7a4e123d76267
Velin3020@students.softuni.bg <Velin3020@students.softuni.bg>
2022-02-14 11:59:03 +0200

    Created RealEstate Entities

---
+++ InsuranceRiskAssessment/InsuranceRiskAssessment.DataAccessLayer/Entities/RealEstate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceRiskAssessment.DataAccessLayer.Entities
+{
+    public abstract class RealEstate : BaseEntity
+    {
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public string City { get; set; }
+        public string Adress { get; set; }
+        public bool FireExtinguishers { get; set; }
+        public bool EmergencyExit { get; set; }
+        public double SquareFeet { get; set; }
+        public bool AlarmSystem { get; set; }
+        public bool GasBottles { get; set; }
+    }
+}
---
+++ InsuranceRiskAssessment/InsuranceRiskAssessment.DataAccessLayer/Entities/RealEstateEntities/BusinessEnterprises.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceRiskAssessment.DataAccessLayer.Entities.RealEstateEntities
+{
+    public class BusinessEnterprises
+    {
+        public string PurposeOfTheEnterprise { get; set; }
+    }
+}

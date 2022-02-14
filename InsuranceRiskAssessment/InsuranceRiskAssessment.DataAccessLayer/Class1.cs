e916cf1587138bf551d84f13c78f1ab5d02a8979
Velin3020@students.softuni.bg <Velin3020@students.softuni.bg>
2022-02-14 10:19:57 +0200

    Initialized Project

---
+++ InsuranceRiskAssessment/InsuranceRiskAssessment.DataAccessLayer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace InsuranceRiskAssessment.DataAccessLayer
+{
+    public class Class1
+    {
+    }
+}
---
+++ InsuranceRiskAssessment/InsuranceRiskAssessment.DataAccessLayer/Data/InsuranceRiskAssessmentDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceRiskAssessment.DataAccessLayer.Data
+{
+    public class InsuranceRiskAssessmentDbContext : IdentityDbContext
+    {
+        public InsuranceRiskAssessmentDbContext(DbContextOptions<InsuranceRiskAssessmentDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}

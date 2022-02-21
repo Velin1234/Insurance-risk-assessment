2728994fc7d81f8037d76af1736036d6559d61f6
Velin3020@students.softuni.bg <Velin3020@students.softuni.bg>
2022-02-21 10:31:50 +0200

    Added Real Estate Entities Services

---
+++ InsuranceRiskAssessment/InsuranceRiskAssessment.BusinessLogicLayer/Abstractions/RealEstateServices/ICommercialPropertyService.cs
@@ -0,0 +1,20 @@
+﻿using InsuranceRiskAssessment.DataAccessLayer.Entities.RealEstateEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceRiskAssessment.BusinessLogicLayer.Abstractions.RealEstateServices
+{
+    public interface ICommercialPropertyService
+    {
+        bool CreateCommercialProperty(string country, string region, string city, string adress, bool fireExtinguishers,
+            bool emergencyExit, double squareFeet, bool alarmSystem, bool gasBottles);
+        bool UpdateCommercialProperty(int commercialPropertyId, string country, string region, string city, string adress, bool fireExtinguishers,
+            bool emergencyExit, double squareFeet, bool alarmSystem, bool gasBottles);
+        List<CommercialProperty> GetCommercialProperty();
+        CommercialProperty GetCommercialPropertyById(int commercialPropertyId);
+        bool Remove(int commercialPropertyId);
+    }
+}
--- InsuranceRiskAssessment/InsuranceRiskAssessment.DataAccessLayer/Entities/RealEstateEntities/VillaBuilding.cs
+++ InsuranceRiskAssessment/InsuranceRiskAssessment.DataAccessLayer/Entities/RealEstateEntities/VillaBuilding.cs
@@ -8,5 +8,6 @@ namespace InsuranceRiskAssessment.DataAccessLayer.Entities.RealEstateEntities
 {
     public class VillaBuilding : RealEstateProperty
     {
+
     }
 }

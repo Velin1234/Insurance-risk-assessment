6a0b6ed264c80ab4b4a073f72db419ff87976159
Velin3020@students.softuni.bg <venito1234@abv.bg>
2022-03-20 17:03:28 +0200

    Fixed conflict DLL problem

--- InsuranceRiskAssessment/InsuranceRiskAssessment.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ InsuranceRiskAssessment/InsuranceRiskAssessment.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -11,19 +11,20 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using InsuranceRiskAssessment.DataAccessLayer.Data;
 
 namespace InsuranceRiskAssessment.Web.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class LoginModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<InsuranceBrokerUser> _userManager;
+        private readonly SignInManager<InsuranceBrokerUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<IdentityUser> signInManager, 
+        public LoginModel(SignInManager<InsuranceBrokerUser> signInManager, 
             ILogger<LoginModel> logger,
-            UserManager<IdentityUser> userManager)
+            UserManager<InsuranceBrokerUser> userManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
@@ -81,7 +82,7 @@ namespace InsuranceRiskAssessment.Web.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, true , lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
--- InsuranceRiskAssessment/InsuranceRiskAssessment.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ InsuranceRiskAssessment/InsuranceRiskAssessment.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using InsuranceRiskAssessment.DataAccessLayer.Data;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -19,14 +20,14 @@ namespace InsuranceRiskAssessment.Web.Areas.Identity.Pages.Account
     [AllowAnonymous]
     public class RegisterModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<InsuranceBrokerUser> _signInManager;
+        private readonly UserManager<InsuranceBrokerUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
 
         public RegisterModel(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager,
+            UserManager<InsuranceBrokerUser> userManager,
+            SignInManager<InsuranceBrokerUser> signInManager,
             ILogger<RegisterModel> logger,
             IEmailSender emailSender)
         {
@@ -74,7 +75,7 @@ namespace InsuranceRiskAssessment.Web.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new InsuranceBrokerUser { UserName = Input.Email, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {

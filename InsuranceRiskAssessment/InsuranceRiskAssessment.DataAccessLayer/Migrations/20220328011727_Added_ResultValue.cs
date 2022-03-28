4a009464c433a635586504d782a74d613528c53c
Velin3020@students.softuni.bg <venito1234@abv.bg>
2022-03-28 04:25:49 +0300

    Reconstructed Folders

---
+++ InsuranceRiskAssessment/InsuranceRiskAssessment.DataAccessLayer/Migrations/20220328011727_Added_ResultValue.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InsuranceRiskAssessment.DataAccessLayer.Migrations
+{
+    public partial class Added_ResultValue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ResultValue",
+                table: "VillaBuildings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ResultValue",
+                table: "VehiclesByLand",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ResultValue",
+                table: "SeaTransports",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ResultValue",
+                table: "ResidentialBuildings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ResultValue",
+                table: "CommercialProperties",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ResultValue",
+                table: "BusinessEnterprises",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ResultValue",
+                table: "AirTransports",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+        }
+    }
+}
--- InsuranceRiskAssessment/InsuranceRiskAssessment.Web/Startup.cs
+++ InsuranceRiskAssessment/InsuranceRiskAssessment.Web/Startup.cs
@@ -1,4 +1,8 @@
+using InsuranceRiskAssessment.BusinessLogicLayer.Abstractions.MovablePropertyServices;
+using InsuranceRiskAssessment.BusinessLogicLayer.Services.MovablePropertyServices;
+using InsuranceRiskAssessment.DataAccessLayer.Abstractions;
 using InsuranceRiskAssessment.DataAccessLayer.Data;
+using InsuranceRiskAssessment.DataAccessLayer.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -25,6 +29,12 @@ namespace InsuranceRiskAssessment
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<InsuranceBrokerUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<InsuranceRiskAssessmentDbContext>();
+
+            services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+            services.AddTransient<IAirTransportService, AirTransportService>();
+            services.AddTransient<ISeaTransportService, SeaTransportService>();
+            services.AddTransient<IVehicleByLandService, VehicleByLandService>();
+            //Add rest services
             services.AddControllersWithViews();
         }
 
